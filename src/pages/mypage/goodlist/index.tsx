9c5082d7565a91d2f193e02fd7c570dbca98bc46
u5u5u5u <kyutech.joko@gmail.com>
2024-03-08 08:43:10 +0900

    add: header_link

--- src/pages/mypage/goodlist/index.tsx
+++ src/pages/mypage/goodlist/index.tsx
@@ -1,28 +1,26 @@
-
 import { Inter } from "next/font/google";
 import styles from "./goodlist.module.css";
 import Link from "next/link";
-import { GoodList } from "@/components/Goodlist";
+import { PostList } from "@/components/Goodlist";
 const inter = Inter({ subsets: ["latin"] });
 
 const mockData = [
-    {
-        id: 1,
-        title: "post1",
-    },
-]
+  {
+    id: 1,
+    title: "post1",
+  },
+];
 export default function Home() {
+  return (
+    <>
+      <div className={styles.title}>
+        <h1>いいねした作品</h1>
+      </div>
 
-    return (
-        <>
-
-            <div className={styles.title}>
-                <h1>いいねした作品</h1>
-            </div>
-
-
-            <Link className={styles.tag} href="/">ギャラリーに戻る</Link>
-            <GoodList />
-        </>
-    )
+      <Link className={styles.tag} href="/">
+        ギャラリーに戻る
+      </Link>
+      <PostList Post={[]} />
+    </>
+  );
 }
