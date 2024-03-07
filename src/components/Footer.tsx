c919b61c5c80301b9e67170cbfccb1b636780b0a
u5u5u5u <kyutech.joko@gmail.com>
2024-03-08 05:13:10 +0900

    add: sns-logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,46 @@
 import React from "react";
+import Image from "next/image";
 import styles from "./Footer.module.css";
 
 export const Footer = () => {
   return (
     <footer className={styles.container}>
-      <p>© 2024 CSS Arts Gallery</p>
+      <p className={styles.logo}>CSS Art Gallery</p>
+      <ul>
+        <li>
+          <Image
+            width={20}
+            height={20}
+            src="/brand-instagram.svg"
+            alt="instagram"
+          />
+        </li>
+        <li>
+          <Image
+            width={20}
+            height={20}
+            src="/brand-twitter-filled.svg"
+            alt="twitter"
+          />
+        </li>
+        <li>
+          <Image
+            width={20}
+            height={20}
+            src="/brand-github-filled.svg"
+            alt="github"
+          />
+        </li>
+        <li>
+          <Image
+            width={20}
+            height={20}
+            src="/brand-discord-filled.svg"
+            alt="discord"
+          />
+        </li>
+      </ul>
+      <p className={styles.c}>© 2024 CSS Arts Gallery</p>
     </footer>
   );
 };
