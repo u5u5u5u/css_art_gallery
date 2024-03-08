5a0f1f6747f59e50f83d15a65b8d6c01a54624b4
Semi-koron <6jimoto6@gmail.com>
2024-03-08 11:55:32 +0900

    add new favorite function

--- src/components/PostButton.tsx
+++ src/components/PostButton.tsx
@@ -1,14 +1,16 @@
 import styles from "./PostButton.module.css";
 import { ReactNode } from "react";
+import clsx from "clsx";
 
 type Props = {
   children: ReactNode;
   onClick?: () => void;
+  className?: string;
 };
 
-export const PostButton = ({ children, onClick }: Props) => {
+export const PostButton = ({ children, onClick, className }: Props) => {
   return (
-    <button className={styles.button} onClick={onClick}>
+    <button className={clsx(styles.button, className)} onClick={onClick}>
       {children}
     </button>
   );
